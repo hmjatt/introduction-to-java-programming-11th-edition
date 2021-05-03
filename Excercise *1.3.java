e6439358a6ef0cd7ec4083887b19e0fec0cc725e
Harmeet Matharoo <39014834+hmjatt@users.noreply.github.com>
2021-05-02 19:50:14 -0700

    Update Excercise *1.3.java

--- Excercise *1.3.java
+++ Excercise *1.3.java
@@ -16,7 +16,7 @@ public class JavaLogo {
         System.out.println("    J    A  V     V  A");
         System.out.println("    J   A A  V   V  A A");
         System.out.println("J   J  AAAAA  V V  AAAAA");
-        System.out.printlna(" JJ   A     A  V  A     A");
+        System.out.println(" JJ   A     A  V  A     A");
     }
 }
 
