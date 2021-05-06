39535053fcdd71c8fca296e131541e551e484c85
Harmeet Matharoo <39014834+hmjatt@users.noreply.github.com>
2021-05-06 00:53:11 -0700

    Update Exercise_*2.8.java

--- Chapter_2/Exercise_*2.8.java
+++ Chapter_2/Exercise_*2.8.java
@@ -1,1 +1,50 @@
-//make changes
+
+/**
+ * CurrentTime.java, gives a program that displays the current time in GMT.Revise the program so it prompts the user to 
+ * enter the time zone offset to GMT and displays the time in the specified time zone.
+ * 
+ * Autor : Harmeet Matharoo
+ * Date : 05 May 2021
+*/
+import java.util.Scanner;
+public class CurrentTime {
+    public static void main(String [] args) {
+        
+        
+        // Obtain the total milliseconds since midnight, Jan 1, 1970 
+        long totalMilliseconds = System.currentTimeMillis(); 
+        
+        // Obtain the total seconds since midnight, Jan 1, 1970 
+        long totalSeconds = totalMilliseconds / 1000; 
+        
+        // Compute the current second in the minute in the hour 
+        long currentSecond = totalSeconds % 60;
+        
+        // Obtain the total minutes 
+        long totalMinutes = totalSeconds / 60; 
+        
+        // Compute the current minute in the hour 
+        long currentMinute = totalMinutes % 60; 
+        
+        // Obtain the total hours 
+        long totalHours = totalMinutes / 60; 
+        
+        // Compute the current hour 
+        long currentHour = totalHours % 24;
+            
+        
+        //Enter the timezone offset to GMT : "ask for input"
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the timezone offset to GMT : ");
+        
+        int offset = input.nextInt();
+        
+        long currentOffsetHour = (totalHours + offset) % 24; 
+    
+        
+        // Display results 
+        System.out.println("Current time is " + currentOffsetHour + ":" + currentMinute + ":" + currentSecond + " GMT");
+        
+    }
+}
